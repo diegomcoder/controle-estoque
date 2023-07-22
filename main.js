60949ca3929f168317d873b70638ffdfa96c65f2
Diego Medeiros <mdsidiego@gmail.com>
2023-07-22 14:53:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/* ETAPAS DE IMPLEMENTAÇÃO DO PROJETO
+
+1. Implementação da leitura de informações dos produtos
+2. Implementação do cadastro de produtos
+3. Implementação do menu de interação
+4. Implementação da consulta de produtos
+5. Implementação do filtro de produtos por preço */
+
